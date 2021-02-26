e9bb8ec6a75f5a3c49891c58f602d9eec2f26deb
Roman Kniazev <kniazev.re@gmail.com>
2021-02-26 20:05:12 +0300

    add arrays

--- scripts/data_types/arrays.js
+++ scripts/data_types/arrays.js
@@ -1,5 +1,5 @@
 // #2 Array operations
-
+/*
 let styles =['Jazz', 'Blues'];
 
 styles.push('Rock-n-Roll');
@@ -12,4 +12,26 @@ if(styles.length % 2) {
 alert(styles.shift());
 
 styles.unshift('Rap', 'Reggae');
-console.log(styles);
+*/
+
+// #4 Sum input numbers
+/*
+function sumInput() {
+  let values = [];
+
+  let value = prompt("Enter the number please!", 0);
+  
+  while (isFinite(value) && value != null){
+    values.push(+value);
+    value = prompt("Enter the number please!", 0);
+  } 
+
+  let result = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    result += values[i];
+  }
+
+  return result;
+}
+*/

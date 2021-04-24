af11fa437d73da26a483f8f2de9f88956758ff58
Roman Kniazev <kniazev.re@gmail.com>
2021-04-24 20:29:14 +0300

    bind

---
+++ scripts/advanced_functions/bind.js
@@ -0,0 +1,41 @@
+// #4 Fix a function that loses "this"
+/*
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'John',
+  
+    loginOk() {
+      alert(`${this.name} logged in`);
+    },
+  
+    loginFail() {
+      alert(`${this.name} failed to log in`);
+    },
+  
+};
+  
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+*/
+
+// #5 Partial application for login
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+}
+  
+let user = {
+name: 'John',
+
+login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+}
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false)); // ?

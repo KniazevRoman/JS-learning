4cdb36f164d1966af868724d693c4a9878835ba6
Roman Kniazev <kniazev.re@gmail.com>
2021-06-17 21:40:22 +0300

    async await

---
+++ scripts/promises/async-await.js
@@ -0,0 +1,14 @@
+// #1 Rewrite using async/await
+
+async function loadJson(url) {
+    let response = await fetch(url);
+
+    if (response.status == 200) {
+        return response.json();
+    } else {
+        throw new Error(response.status);;
+    }
+}
+  
+loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
